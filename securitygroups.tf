0a21f11495425d68ffb4a36ee8911e07d595be83
Johnson Zhang <johnson.zhang@dataiku.com>
2022-03-01 17:48:38 +1030

    add egress rule

--- securitygroups.tf
+++ securitygroups.tf
@@ -18,3 +18,12 @@ resource "aws_security_group_rule" "allow_ssh" {
   cidr_blocks       = [var.my_ip,]
   security_group_id = aws_security_group.allow_ssh.id
 }
+
+resource "aws_security_group_rule" "allow_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0",]
+  security_group_id = aws_security_group.allow_ssh.id
+}
