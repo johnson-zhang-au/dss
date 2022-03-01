c699e3d598c7f65c3733281dd594f4f5dcb44516
Johnson Zhang <johnson.zhang@dataiku.com>
2022-03-01 18:16:06 +1030

    add rt

--- network.tf
+++ network.tf
@@ -37,3 +37,14 @@ resource "aws_internet_gateway" "gw" {
   )
 }
 
+resource "aws_route_table" "jz-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+
+   tags = local.common_tags
+}
