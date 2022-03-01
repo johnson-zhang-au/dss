09b969ebe75ad29c60be602d1b5b2110c21343cf
Johnson Zhang <johnson.zhang@dataiku.com>
2022-03-01 22:17:43 +1030

    add ingress rule for port 80 and 443

--- securitygroups.tf
+++ securitygroups.tf
@@ -19,6 +19,24 @@ resource "aws_security_group_rule" "allow_ssh" {
   security_group_id = aws_security_group.allow_ssh.id
 }
 
+resource "aws_security_group_rule" "allow_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_ssh.id
+}
+
+resource "aws_security_group_rule" "allow_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_ssh.id
+}
+
 resource "aws_security_group_rule" "allow_all" {
   type              = "egress"
   from_port         = 0
