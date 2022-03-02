a8db0bdea56213ce932a6e08548bcb67d5744023
Johnson Zhang <johnson.zhang@dataiku.com>
2022-03-02 10:32:45 +1030

    fix syntax issue

--- instanceprofile.tf
+++ instanceprofile.tf
@@ -8,8 +8,21 @@ resource "aws_iam_role" "dss_role" {
   name = "johnsonz_dss_role"
   path = "/"
 
-  assume_role_policy  = data.aws_iam_policy_document.instance-assume-role-policy.json
-  managed_policy_arns = [aws_iam_policy.policy_one.arn, ]
+  assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
+  managed_policy_arns = [
+    aws_iam_policy.policy_dss_ecr.arn,
+    aws_iam_policy.policy_dss_eks.arn,
+    aws_iam_policy.policy_dss_eksctl_cf.arn,
+    aws_iam_policy.policy_dss_eks_launch_conf.arn,
+    aws_iam_policy.policy_dss_eks_autoscale.arn,
+    aws_iam_policy.policy_dss_eks_iam.arn,
+    aws_iam_policy.policy_dss_eks_linked_role.arn,
+    aws_iam_policy.policy_dss_eks_iam_openid.arn,
+    aws_iam_policy.policy_dss_eks_igw.arn,
+    aws_iam_policy.policy_dss_eks_launch_template.arn,
+    aws_iam_policy.policy_dss_eks_networking.arn,
+    aws_iam_policy.policy_dss_eks_ssm.arn,
+  ]
 
 }
 
@@ -41,7 +54,7 @@ resource "aws_iam_policy" "policy_dss_ecr" {
           "ecr:InitiateLayerUpload",
           "ecr:PutImage",
           "ecr:UploadLayerPart",
-          "ecr:CreateRepository" ]
+        "ecr:CreateRepository"]
         Effect   = "Allow"
         Resource = "*"
       },
@@ -57,11 +70,11 @@ resource "aws_iam_policy" "policy_dss_eks" {
     Version = "2012-10-17"
     Statement = [
       {
-      "Sid": "EksActions",
-      "Effect": "Allow",
-      "Action": "eks:*",
-      "Resource": "*"
-    },
+        "Sid" : "EksActions",
+        "Effect" : "Allow",
+        "Action" : "eks:*",
+        "Resource" : "*"
+      },
     ]
   })
 }
@@ -74,11 +87,11 @@ resource "aws_iam_policy" "policy_dss_eksctl_cf" {
     Version = "2012-10-17"
     Statement = [
       {
-      "Sid": "eksctlCloudFormation",
-      "Effect": "Allow",
-      "Action": "cloudformation:*",
-      "Resource": "*"
-    }
+        "Sid" : "eksctlCloudFormation",
+        "Effect" : "Allow",
+        "Action" : "cloudformation:*",
+        "Resource" : "*"
+      }
     ]
   })
 }
@@ -91,14 +104,14 @@ resource "aws_iam_policy" "policy_dss_eks_launch_conf" {
     Version = "2012-10-17"
     Statement = [
       {
-      "Sid": "EksLaunchConfigs",
-      "Effect": "Allow",
-      "Action": [
-        "autoscaling:CreateLaunchConfiguration",
-        "autoscaling:DeleteLaunchConfiguration"
-      ],
-      "Resource": "arn:aws:autoscaling:*:*:launchConfiguration:*:launchConfigurationName/*"
-    }
+        "Sid" : "EksLaunchConfigs",
+        "Effect" : "Allow",
+        "Action" : [
+          "autoscaling:CreateLaunchConfiguration",
+          "autoscaling:DeleteLaunchConfiguration"
+        ],
+        "Resource" : "arn:aws:autoscaling:*:*:launchConfiguration:*:launchConfigurationName/*"
+      }
     ]
   })
 }
@@ -110,25 +123,25 @@ resource "aws_iam_policy" "policy_dss_eks_autoscale" {
     Version = "2012-10-17"
     Statement = [
       {
-      "Sid": "EksAutoScaleWrite",
-      "Effect": "Allow",
-      "Action": [
-        "autoscaling:UpdateAutoScalingGroup",
-        "autoscaling:DeleteAutoScalingGroup",
-        "autoscaling:CreateAutoScalingGroup"
-      ],
-      "Resource": "arn:aws:autoscaling:*:*:autoScalingGroup:*:autoScalingGroupName/*"
-    },
-    {
-      "Sid": "EksAutoScale",
-      "Effect": "Allow",
-      "Action": [
-        "autoscaling:DescribeAutoScalingGroups",
-        "autoscaling:DescribeScalingActivities",
-        "autoscaling:DescribeLaunchConfigurations"
-      ],
-      "Resource": "*"
-    }
+        "Sid" : "EksAutoScaleWrite",
+        "Effect" : "Allow",
+        "Action" : [
+          "autoscaling:UpdateAutoScalingGroup",
+          "autoscaling:DeleteAutoScalingGroup",
+          "autoscaling:CreateAutoScalingGroup"
+        ],
+        "Resource" : "arn:aws:autoscaling:*:*:autoScalingGroup:*:autoScalingGroupName/*"
+      },
+      {
+        "Sid" : "EksAutoScale",
+        "Effect" : "Allow",
+        "Action" : [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeScalingActivities",
+          "autoscaling:DescribeLaunchConfigurations"
+        ],
+        "Resource" : "*"
+      }
     ]
   })
 }
@@ -140,32 +153,32 @@ resource "aws_iam_policy" "policy_dss_eks_iam" {
     Version = "2012-10-17"
     Statement = [
       {
-      "Sid": "EksIam",
-      "Effect": "Allow",
-      "Action": [
-        "iam:AddRoleToInstanceProfile",
-        "iam:AttachRolePolicy",
-        "iam:CreateInstanceProfile",
-        "iam:CreateRole",
-        "iam:DeleteInstanceProfile",
-        "iam:DeleteRole",
-        "iam:DeleteRolePolicy",
-        "iam:DetachRolePolicy",
-        "iam:GetInstanceProfile",
-        "iam:GetRole",
-        "iam:GetRolePolicy",
-        "iam:ListAttachedRolePolicies",
-        "iam:ListInstanceProfiles",
-        "iam:ListInstanceProfilesForRole",
-        "iam:PassRole",
-        "iam:PutRolePolicy",
-        "iam:RemoveRoleFromInstanceProfile"
-      ],
-      "Resource": [
-        "arn:aws:iam::${locals.current_account_id}:instance-profile/eksctl-*",
-        "arn:aws:iam::${locals.current_account_id}:role/eksctl-*"
-      ]
-    }
+        "Sid" : "EksIam",
+        "Effect" : "Allow",
+        "Action" : [
+          "iam:AddRoleToInstanceProfile",
+          "iam:AttachRolePolicy",
+          "iam:CreateInstanceProfile",
+          "iam:CreateRole",
+          "iam:DeleteInstanceProfile",
+          "iam:DeleteRole",
+          "iam:DeleteRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:GetInstanceProfile",
+          "iam:GetRole",
+          "iam:GetRolePolicy",
+          "iam:ListAttachedRolePolicies",
+          "iam:ListInstanceProfiles",
+          "iam:ListInstanceProfilesForRole",
+          "iam:PassRole",
+          "iam:PutRolePolicy",
+          "iam:RemoveRoleFromInstanceProfile"
+        ],
+        "Resource" : [
+          "arn:aws:iam::${local.current_account_id}:instance-profile/eksctl-*",
+          "arn:aws:iam::${local.current_account_id}:role/eksctl-*"
+        ]
+      }
     ]
   })
 }
@@ -177,14 +190,14 @@ resource "aws_iam_policy" "policy_dss_eks_linked_role" {
     Version = "2012-10-17"
     Statement = [
       {
-      "Sid": "EksIamLinkedRole",
-      "Effect": "Allow",
-      "Action": "iam:CreateServiceLinkedRole",
-      "Resource": [
-        "arn:aws:iam::${locals.current_account_id}:role/aws-service-role/eks.amazonaws.com/*",
-        "arn:aws:iam::${locals.current_account_id}:role/aws-service-role/autoscaling.amazonaws.com/*"
-      ]
-    }
+        "Sid" : "EksIamLinkedRole",
+        "Effect" : "Allow",
+        "Action" : "iam:CreateServiceLinkedRole",
+        "Resource" : [
+          "arn:aws:iam::${local.current_account_id}:role/aws-service-role/eks.amazonaws.com/*",
+          "arn:aws:iam::${local.current_account_id}:role/aws-service-role/autoscaling.amazonaws.com/*"
+        ]
+      }
     ]
   })
 }
@@ -196,11 +209,11 @@ resource "aws_iam_policy" "policy_dss_eks_iam_openid" {
     Version = "2012-10-17"
     Statement = [
       {
-      "Sid": "EksIamOpenID",
-      "Effect": "Allow",
-      "Action": "iam:GetOpenIDConnectProvider",
-      "Resource": "arn:aws:iam::${locals.current_account_id}:oidc-provider/oidc.eks.*"
-    }
+        "Sid" : "EksIamOpenID",
+        "Effect" : "Allow",
+        "Action" : "iam:GetOpenIDConnectProvider",
+        "Resource" : "arn:aws:iam::${local.current_account_id}:oidc-provider/oidc.eks.*"
+      }
     ]
   })
 }
@@ -212,11 +225,11 @@ resource "aws_iam_policy" "policy_dss_eks_igw" {
     Version = "2012-10-17"
     Statement = [
       {
-      "Sid": "EksInternetGateway",
-      "Effect": "Allow",
-      "Action": "ec2:DeleteInternetGateway",
-      "Resource": "arn:aws:ec2:*:*:internet-gateway/*"
-    }
+        "Sid" : "EksInternetGateway",
+        "Effect" : "Allow",
+        "Action" : "ec2:DeleteInternetGateway",
+        "Resource" : "arn:aws:ec2:*:*:internet-gateway/*"
+      }
     ]
   })
 }
@@ -227,11 +240,11 @@ resource "aws_iam_policy" "policy_dss_eks_launch_template" {
     Version = "2012-10-17"
     Statement = [
       {
-      "Sid": "EksLaunchTemplates",
-      "Effect": "Allow",
-      "Action": "ec2:*LaunchTemplate*",
-      "Resource": "*"
-    }
+        "Sid" : "EksLaunchTemplates",
+        "Effect" : "Allow",
+        "Action" : "ec2:*LaunchTemplate*",
+        "Resource" : "*"
+      }
     ]
   })
 }
@@ -242,49 +255,49 @@ resource "aws_iam_policy" "policy_dss_eks_networking" {
     Version = "2012-10-17"
     Statement = [
       {
-      "Sid": "EksNetworking",
-      "Effect": "Allow",
-      "Action": [
-        "ec2:AuthorizeSecurityGroupIngress",
-        "ec2:DeleteSubnet",
-        "ec2:DeleteTags",
-        "ec2:CreateNatGateway",
-        "ec2:CreateVpc",
-        "ec2:AttachInternetGateway",
-        "ec2:DescribeVpcAttribute",
-        "ec2:DeleteRouteTable",
-        "ec2:AssociateRouteTable",
-        "ec2:DescribeVpcs",
-        "ec2:DescribeInternetGateways",
-        "ec2:CreateRoute",
-        "ec2:CreateInternetGateway",
-        "ec2:RevokeSecurityGroupEgress",
-        "ec2:CreateSecurityGroup",
-        "ec2:ModifyVpcAttribute",
-        "ec2:DeleteInternetGateway",
-        "ec2:DescribeRouteTables",
-        "ec2:ReleaseAddress",
-        "ec2:AuthorizeSecurityGroupEgress",
-        "ec2:DescribeTags",
-        "ec2:CreateTags",
-        "ec2:DeleteRoute",
-        "ec2:CreateRouteTable",
-        "ec2:DetachInternetGateway",
-        "ec2:DescribeNatGateways",
-        "ec2:DisassociateRouteTable",
-        "ec2:AllocateAddress",
-        "ec2:DescribeSecurityGroups",
-        "ec2:RevokeSecurityGroupIngress",
-        "ec2:DeleteSecurityGroup",
-        "ec2:DeleteNatGateway",
-        "ec2:DeleteVpc",
-        "ec2:CreateSubnet",
-        "ec2:DescribeSubnets",
-        "ec2:DescribeImages",
-        "ec2:RunInstances"
-      ],
-      "Resource": "*"
-    }
+        "Sid" : "EksNetworking",
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:DeleteSubnet",
+          "ec2:DeleteTags",
+          "ec2:CreateNatGateway",
+          "ec2:CreateVpc",
+          "ec2:AttachInternetGateway",
+          "ec2:DescribeVpcAttribute",
+          "ec2:DeleteRouteTable",
+          "ec2:AssociateRouteTable",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeInternetGateways",
+          "ec2:CreateRoute",
+          "ec2:CreateInternetGateway",
+          "ec2:RevokeSecurityGroupEgress",
+          "ec2:CreateSecurityGroup",
+          "ec2:ModifyVpcAttribute",
+          "ec2:DeleteInternetGateway",
+          "ec2:DescribeRouteTables",
+          "ec2:ReleaseAddress",
+          "ec2:AuthorizeSecurityGroupEgress",
+          "ec2:DescribeTags",
+          "ec2:CreateTags",
+          "ec2:DeleteRoute",
+          "ec2:CreateRouteTable",
+          "ec2:DetachInternetGateway",
+          "ec2:DescribeNatGateways",
+          "ec2:DisassociateRouteTable",
+          "ec2:AllocateAddress",
+          "ec2:DescribeSecurityGroups",
+          "ec2:RevokeSecurityGroupIngress",
+          "ec2:DeleteSecurityGroup",
+          "ec2:DeleteNatGateway",
+          "ec2:DeleteVpc",
+          "ec2:CreateSubnet",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeImages",
+          "ec2:RunInstances"
+        ],
+        "Resource" : "*"
+      }
     ]
   })
 }
@@ -295,18 +308,18 @@ resource "aws_iam_policy" "policy_dss_eks_ssm" {
     Version = "2012-10-17"
     Statement = [
       {
-      "Sid": "SsmOfficialImagesFetch",
-      "Effect": "Allow",
-      "Action": [
-        "ssm:GetParameter",
-        "ssm:GetParameters"
-      ],
-      "Resource": [
-        "arn:aws:ssm:*:*:parameter/aws/service/eks/optimized-ami/*/amazon-linux-2/recommended/image_id",
-        "arn:aws:ssm:*:*:parameter/aws/service/eks/optimized-ami/*/amazon-linux-2/recommended/image_id",
-        "*"
-      ]
-    }
+        "Sid" : "SsmOfficialImagesFetch",
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm:GetParameter",
+          "ssm:GetParameters"
+        ],
+        "Resource" : [
+          "arn:aws:ssm:*:*:parameter/aws/service/eks/optimized-ami/*/amazon-linux-2/recommended/image_id",
+          "arn:aws:ssm:*:*:parameter/aws/service/eks/optimized-ami/*/amazon-linux-2/recommended/image_id",
+          "*"
+        ]
+      }
     ]
   })
 }
