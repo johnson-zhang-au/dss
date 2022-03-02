97ff2b967934aaf9a461113db86abd34b47a09de
Johnson Zhang <johnson.zhang@dataiku.com>
2022-03-02 11:23:58 +1030

    modify subnet az

--- network.tf
+++ network.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "main" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.vpc_subnets[count.index].cidr_block
   map_public_ip_on_launch = true
-  availability_zone= "${data.aws_availability_zones.available.names[(count.index+3) % length(data.aws_availability_zones.available.names)]}"
+  availability_zone= "${data.aws_availability_zones.available.names[(count.index+2) % length(data.aws_availability_zones.available.names)]}"
 
   tags = merge(
     local.common_tags,
